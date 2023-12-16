db79452c46b28f03c5c2ee186998e43b33e28816
Dennis Tian <dtian88@gmail.com>
2023-12-15 23:46:18 -0500

    Day 15 update

--- 2023/AoC_2023_15.py
+++ 2023/AoC_2023_15.py
@@ -1,5 +1,3 @@
-from collections import OrderedDict
-
 def hash_algorithm(s):
   h = 0
   for c in s:
@@ -9,19 +7,15 @@ def hash_algorithm(s):
 
 data = open("AoC_2023_15.txt").read().split(",")
 part1 = part2 = 0
-boxes = [OrderedDict() for _ in range(256)]
+boxes = [{} for _ in range(256)]
 
 for step in data:
   part1 += hash_algorithm(step)
   label = step[:-1 if step[-1] == "-" else -2]
-  h = hash_algorithm(label)
+  num = hash_algorithm(label)
   if step[-1] == "-":
-    boxes[h].pop(label, None)
+    boxes[num].pop(label, None)
   else:
-    boxes[h][label] = int(step[-1])
+    boxes[num][label] = int(step[-1])
 
-for box_num, box in enumerate(boxes):
-  for slot_num, focal_length in enumerate(box.values()):
-    part2 += (box_num + 1) * (slot_num + 1) * focal_length
-  
-print("Part 1: %d; part 2: %d" % (part1, part2))
+print("Part 1: %d; part 2: %d" % (part1, sum(sum((num + 1) * (slot + 1) * focal_length for slot, focal_length in enumerate(box.values())) for num, box in enumerate(boxes))))
